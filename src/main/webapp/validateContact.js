3553053bb1c1a5d77ec10b549db1feb8f2d564bc
Ryan Ho <yu.wae@hotmail.com>
2023-11-23 23:25:52 +0800

    Update validation check for name input

--- src/main/webapp/validateContact.js
+++ src/main/webapp/validateContact.js
@@ -1,3 +1,8 @@
+function validateNameInput(name) {
+  const nameVal = /^[A-Za-z\s]+$/;
+  return nameVal.test(name);
+}
+
 let name = document.getElementById("name");
 let email = document.getElementById("email");
 let message = document.getElementById("message");
@@ -8,6 +13,8 @@ form.addEventListener("submit", (e)=>{
 	
 	if(name.value.trim()===""){
 		errors.push("Name required.");
+	}else if (!validateNameInput(name.value.trim())){
+		errors.push("Invalid name format. Please use only alphabets and spaces");
 	}
 	
 	if(email.value.trim()===""){
