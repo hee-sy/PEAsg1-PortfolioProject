3dc1387b91589c22116677ec1323cdc30cc60457
Fletcher032 <95692933+Fletcher032@users.noreply.github.com>
2023-11-24 15:14:30 +0800

    Code Formatting (completed)

--- src/main/webapp/tailwind-config.js
+++ src/main/webapp/tailwind-config.js
@@ -1,24 +1,24 @@
 
 tailwind.config = {
-		theme : {
-			extend : {
-				colors : {
-					clifford : "#da373d",
-					buttonLeft : "#e1c2f0",
-					buttonRight : "#6897fa",
-                    white : "#ffffff",
+	theme: {
+		extend: {
+			colors: {
+				clifford: "#da373d",
+				buttonLeft: "#e1c2f0",
+				buttonRight: "#6897fa",
+				white: "#ffffff",
 				// background white color: bg-slate-50
-				},
-			},
-			fontFamily : {
-				// add your fonts here
-				comfortaa : [ "Comfortaa", "sans-serif" ],
-				nunito : [ "Nunito", "sans-serif" ],
-				outfit : [ "Outfit", "sans-serif" ],
-				playfair : [ "Playfair Display", "serif" ],
-				poppins : [ "Poppins", "sans-serif" ],
-				raleway : [ "Raleway", "sans-serif" ],
-				roboto : [ "Roboto", "sans-serif" ],
 			},
 		},
-	};
+		fontFamily: {
+			// add your fonts here
+			comfortaa: ["Comfortaa", "sans-serif"],
+			nunito: ["Nunito", "sans-serif"],
+			outfit: ["Outfit", "sans-serif"],
+			playfair: ["Playfair Display", "serif"],
+			poppins: ["Poppins", "sans-serif"],
+			raleway: ["Raleway", "sans-serif"],
+			roboto: ["Roboto", "sans-serif"],
+		},
+	},
+};
--- src/main/webapp/validateContact.js
+++ src/main/webapp/validateContact.js
@@ -1,6 +1,6 @@
 function validateNameInput(name) {
-  const nameVal = /^[A-Za-z\s]+$/;
-  return nameVal.test(name);
+	const nameVal = /^[A-Za-z\s]+$/;
+	return nameVal.test(name);
 }
 
 let name = document.getElementById("name");
@@ -8,24 +8,24 @@ let email = document.getElementById("email");
 let message = document.getElementById("message");
 let form = document.querySelector("form");
 
-form.addEventListener("submit", (e)=>{
+form.addEventListener("submit", (e) => {
 	const errors = [];
-	
-	if(name.value.trim()===""){
+
+	if (name.value.trim() === "") {
 		errors.push("Name required.");
-	}else if (!validateNameInput(name.value.trim())){
+	} else if (!validateNameInput(name.value.trim())) {
 		errors.push("Invalid name format. Please use only alphabets and spaces");
 	}
-	
-	if(email.value.trim()===""){
+
+	if (email.value.trim() === "") {
 		errors.push("Email required.");
 	}
-	
-	if(message.value.trim()===""){
+
+	if (message.value.trim() === "") {
 		errors.push("Please write a message.");
 	}
-	
-	if(errors.length > 0){
+
+	if (errors.length > 0) {
 		e.preventDefault();
 		alert(errors.join('\n'))
 	}
